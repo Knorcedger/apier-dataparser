3c32924684252ef83b3457731f230db089738659
Achilleas Tsoumitas <knorcedger@gmail.com>
2016-03-04 14:46:47 +0200

    Now properly reads get request data

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-// var url = require('url');
+var url = require('url');
 var _ = require('lodash');
 var reqlog = require('reqlog');
 
 // Reads the request data and saves them on req.requestData
 module.exports = function(req, res, next) {
+	req.query = url.parse(req.url, true).query;
 	if (req.method === 'POST' || req.method === 'PUT') {
 		req.requestData = _.extend(req.body, req.query);
 	} else if (req.method === 'GET') {
